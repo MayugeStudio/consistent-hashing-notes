cf57eccf6bdda09f1e4b0bdfd61cec483eb1db80
Kawahara <My.favorite.is.energy@gmail.com>
2026-05-11 21:48:17 +0900

    [fmt] go fmt

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"hash/fnv"
 	"hash/crc32"
+	"hash/fnv"
 )
 
-
 const (
-	N = 50    // the number of the servers
+	N = 50 // the number of the servers
 	// M = 65393 // the length of the look-up-table
 	M = 1_000_003
 )
@@ -18,23 +17,22 @@ type LookUpTable [M]string
 
 // Hash functions
 func hashStringToInt_1(s string) uint32 {
-    h := fnv.New32a()
-    h.Write([]byte(s))
-    return h.Sum32()
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
 }
 
 func hashStringToInt_2(s string) uint32 {
 	return crc32.ChecksumIEEE([]byte(s))
 }
 
-
 func GeneratePermutationList(names [N]string) Permutations {
 	var permutations Permutations
-	for i := 0; i<N; i++ {
+	for i := 0; i < N; i++ {
 		name := names[i]
 		offset := int(hashStringToInt_1(name) % M)
-		skip := int(hashStringToInt_2(name) % (M-1)) + 1
-		for j := 0; j<M; j++ {
+		skip := int(hashStringToInt_2(name)%(M-1)) + 1
+		for j := 0; j < M; j++ {
 			permutations[i][j] = (offset + j*skip) % M
 		}
 	}
@@ -43,19 +41,19 @@ func GeneratePermutationList(names [N]string) Permutations {
 
 func PopulateLookUpTable(names [N]string, permutations Permutations) [M]int {
 	var next [N]int
-	for i := 0; i<N; i++ {
+	for i := 0; i < N; i++ {
 		next[i] = 0
 	}
 
 	var entry [M]int
-	for i := 0; i<M; i++ {
+	for i := 0; i < M; i++ {
 		entry[i] = -1
 	}
 
 	n := 0
 
 	for {
-		for i := 0; i<N; i++ {
+		for i := 0; i < N; i++ {
 			c := permutations[i][next[i]]
 			if c == -1 {
 				continue
@@ -76,20 +74,19 @@ func PopulateLookUpTable(names [N]string, permutations Permutations) [M]int {
 }
 
 func MarkServerAsDown(index int, permutations *Permutations) {
-	for i := 0; i<M; i++ {
+	for i := 0; i < M; i++ {
 		permutations[index][i] = -1
 	}
 }
 
-
 func main() {
 	var names [N]string
-	for i := 0; i<N; i++ {
+	for i := 0; i < N; i++ {
 		names[i] = fmt.Sprintf("SERVER%d", i)
 	}
 
 	permutations := GeneratePermutationList(names)
-	
+
 	lookUpTable := PopulateLookUpTable(names, permutations)
 	MarkServerAsDown(0, &permutations) // Mark server 0 as down
 	lookUpTable2 := PopulateLookUpTable(names, permutations)
